06079833e8a31829ef6556fc48f22252fb682376
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2024-05-18 00:46:07 +0700

    Fix typo

--- app/Models/Pesan.php
+++ app/Models/Pesan.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class pesan extends Model
+class Pesan extends Model
 {
     use HasFactory;
 
