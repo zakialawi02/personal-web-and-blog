ad28196e826ce80a069ce309ff7e614d973fd093
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2024-05-22 00:00:11 +0700

    Fix comment view system

--- resources/views/components/front/navbar.blade.php
+++ resources/views/components/front/navbar.blade.php
@@ -18,7 +18,7 @@
         <nav :class="[isOpen ? 'block' : 'hidden md:flex']" id="nav-menu" class="absolute left-0 right-0 flex flex-col p-3 text-[1.1rem] font-semibold md:relative top-20 md:flex-row md:opacity-100 md:top-0 md:p-0 text-dark uppercase bg-base-100 md:bg-transparent z-10">
             <a class="p-2 duration-300 hover:text-accent" href="{{ route("article.index") }}">Blog</a>
             <a class="p-2 duration-300 hover:text-accent" href="/#about">About</a>
-            <a class="p-2 duration-300 hover:text-accent" href="#">Contact</a>
+            <a class="p-2 duration-300 hover:text-accent" href="/contact-me">Contact</a>
             <div class="flex flex-col items-start gap-2 ml-2 md:items-center md:flex-row">
                 <a class="p-1 px-4 duration-300 border-2 border-secondary rounded-xl hover:bg-light hover:text-secondary bg-secondary text-light" href="#">Gallery</a>
 
